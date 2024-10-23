13b9dd93c540483500c4c9df98dfc2448f2ae4d8
W1ct2 <renskove@gmail.com>
2024-10-24 00:49:04 +0300

    commit 4 (slider & menu list(1,2.3))

---
+++ main.js
@@ -0,0 +1,17 @@
+new Swiper('.swiper', {
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    speed: 2000,
+    slidesPerView: 3,
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: -300,
+        slideShadows: false,
+        scale: 0.8,
+        depth: 200,
+    },
+});
