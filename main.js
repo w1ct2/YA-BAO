08fc999eee1fcdeadc72a52a2c0fa0a861bdcf60
W1ct2 <renskove@gmail.com>
2024-11-12 01:26:36 +0300

    commit 14 (Purchase Page - waiting for completion)

--- main.js
+++ main.js
@@ -15,25 +15,6 @@ new Swiper('.prewiev__slider', {
         depth: 200,
     },
     simulateTouch: false,
-    // breakpoints: {
-    //     700: {
-    //         grabCursor: true,
-    //         loop: true,
-    //         autoplay: {
-    //             delay: 3000,
-    //         },
-    //         speed: 2000,
-    //         slidesPerView: 3,
-    //         effect: 'coverflow',
-    //         coverflowEffect: {
-    //             rotate: 0,
-    //             stretch: 0,
-    //             slideShadows: false,
-    //             scale: 0.8,
-    //             depth: 200,
-    //         },
-    //     }
-    // }
 });
 new Swiper('.options', {
     loop: true,
@@ -43,23 +24,41 @@ new Swiper('.options', {
         sensitivity: 1,
     },
 });
+new Swiper ('.purchase__page-third__choice', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    mousewheel: {
+        sensitivity: 1,
+    },
+    direction: 'vertical',
+    spaceBetween: 150,
+})
 
 const burgerBtn = document.querySelector('.burger__btn');
 const burgerOptions = document.querySelector('.burger__options');
 const body = document.querySelector('body')
 const blackout = document.querySelector('.blackout')
 const burgerClose = document.querySelector('.burger__close')
+const menuCardBasket = document.querySelector('.menu__card__basket')
+const purchasePage = document.querySelector('.purchase__page')
 burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('active')
     burgerOptions.classList.toggle('active')
     body.classList.toggle('active')
     blackout.classList.toggle('active')
 })
+menuCardBasket.addEventListener('click', () => {
+    body.classList.toggle('active')
+    blackout.classList.toggle('active')
+    purchasePage.classList.toggle('active')
+})
+
 blackout.addEventListener('click', () => {
     burgerOptions.classList.remove('active')
     blackout.classList.remove('active')
     burgerBtn.classList.remove('active')
     body.classList.remove('active')
+    purchasePage.classList.remove('active')
 })
 burgerClose.addEventListener('click', () => {
     burgerOptions.classList.remove('active')
